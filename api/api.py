bcc3ba57d67ed4410327372d5d6c115ec46474b8
jjackson112 <jjackson11211@gmail.com>
2025-04-25 17:01:03 -0400

    DB Model

--- api/api.py
+++ api/api.py
@@ -9,5 +9,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
 # create database object by calling SQL Alchemy class
 db = SQLAlchemy(app)
 
+# create a db model to organize database
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    ingredients = db.Column(db.String(500), nullable=False)
+    instructions = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=True, default='Delicious. You need to try it!')
+    image_url = db.Column(db.String(500), nullable=True, default="https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
+    servings = db.Column(db.Integer, nullable=False)
+    
+    def __repr__(self):
+        return f"Recipe(id={self.id}, title='{self.title}', description='{self.description}', servings={self.servings})"
+
 if __name__ == '__main__':
     app.run(debug=True)
