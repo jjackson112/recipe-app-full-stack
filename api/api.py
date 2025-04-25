71624b24d52bff7064cb56dc21da25e4d758ae89
jjackson112 <jjackson11211@gmail.com>
2025-04-25 16:59:01 -0400

    set up database

--- api/api.py
+++ api/api.py
@@ -4,6 +4,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# set up SQL database - location configured to store the database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
+# create database object by calling SQL Alchemy class
+db = SQLAlchemy(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
