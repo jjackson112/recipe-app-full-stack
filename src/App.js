fb5b9d74acceb30874d8ff8f5676699fc80db694
jjackson112 <jjackson11211@gmail.com>
2025-04-26 09:18:32 -0400

    Async Function to show recipes on the front end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import "./App.css";
 
+/* async request to /api/recipes endpoint to grab all recipes and update state, handle errors, and make sure response is ok*/
+
 function App() {
+  const [recipes, setRecipes] = useState([])
+
+  useEffect( () => {
+    const fetchAllRecipes = async () => {
+      try {
+        const response = await fetch('/api/recipes')
+        if (response.ok) {
+          const data = await response.json();
+          setRecipes(data);
+        } else {
+          console.log(`Cannot find that recipe!`);
+        }
+      } catch (e) {
+        console.log(`An error has occurred. Please try again.`)
+      }
+    };
+    fetchAllRecipes();
+  }, [])
+
   return (
     <div className='recipe-app'>
       <Header />
       <p>Your recipes here! </p>
+      {JSON.stringify(recipes)}
     </div>
   );
 }
