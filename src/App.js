ae51412bbd301f5560b416ee9a7516b59d58183a
jjackson112 <jjackson11211@gmail.com>
2025-04-28 16:59:39 -0400

    Show more state - show or hide new recipe form

--- src/App.js
+++ src/App.js
@@ -4,12 +4,25 @@ import Header from "./components/Header";
 import "./App.css";
 import RecipeExcerpt from "./components/RecipeExcerpt.js";
 import RecipeFull from "./components/RecipeFull.js"
+import NewRecipeForm from "./components/NewRecipeForm.js";
 
 /* async request to /api/recipes endpoint to grab all recipes and update state, handle errors, and make sure response is ok*/
 
 function App() {
   const [recipes, setRecipes] = useState([])
   const [selectedRecipe, setSelectedRecipe] = useState(null)
+  // there is no id attribute since the database assigns one by default for each form submission
+  const [newRecipe, setNewRecipe] = useState(
+    {
+      title: "",
+      ingredients: "",
+      instructions: "",
+      servings: 1, // conservative default
+      description: "",
+      image_url: "https://images.pexels.com/photos/9986228/pexels-photo-9986228.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" //default
+    }
+  )
+  const [showNewRecipeForm, setShowNewRecipeForm] = useState(false)
 
   useEffect( () => {
     const fetchAllRecipes = async () => {
@@ -39,11 +52,26 @@ function App() {
       setSelectedRecipe(null);
     };
 
+  /* Show state of a new submitted recipe */
+  /* Set selectedRecipe back to null so you can show the recipe form OR the selected recipe */
+  const showRecipeForm = () => {
+    showNewRecipeForm(true)
+    selectedRecipe(null)
+  }
+
+  /* Hide state of a new submitted recipe */
+  const hideRecipeForm = () => {
+    showNewRecipeForm(false)
+  }
+
   /* replace {JSON.stringify(recipes)} with a map over all recipes in state*/
   /* each recipe has an ID for the key and a prop for each recipe*/
   return (
     <div className='recipe-app'>
-      <Header />
+      <Header showRecipeForm={showRecipeForm}/>
+      {showNewRecipeForm && (
+        <NewRecipeForm newRecipe={newRecipe} hideRecipeForm={hideRecipeForm}/>
+      )}
       {selectedRecipe && (
         <RecipeFull selectedRecipe={selectedRecipe} handleUnselectRecipe={handleUnselectRecipe}/>
       )}
--- src/components/NewRecipeForm.js
+++ src/components/NewRecipeForm.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const NewRecipeForm = () => {
+const NewRecipeForm = (newRecipe, hideRecipeForm) => {
     
     return (
        <div className="recipe-details">
