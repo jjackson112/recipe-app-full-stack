da7df5c6247bf66dac1ccbaebb1efffd53daf5cc
jjackson112 <jjackson11211@gmail.com>
2025-05-01 13:36:22 -0400

    delete recipes function + ConfirmationModal.js added

---
+++ src/components/ConfirmationModal.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ConfirmationModal = () => {
+    return (
+        <div className="confirm">
+            <div className="confirm-content">
+                <p></p>
+                <button className="cancel-button">Cancel</button>
+                <button className="delete-button">Delete</button>
+            </div>
+        </div>
+    )
+}
+
+export default ConfirmationModal;
