11fb8a782086cd453a55eade7dce98b7a503fc01
jjackson112 <jjackson11211@gmail.com>
2025-06-04 17:00:09 -0400

    Updated backend for JWT

--- frontend/src/components/AuthContext.js
+++ frontend/src/components/AuthContext.js
@@ -8,6 +8,8 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null); // e.g., { username: 'testuser' }
     const [token, setToken] = useState(() => localStorage.getItem('token'));
 
+    // decodedToken.sub is not a string, it's actually an object
+    // pay attention to how the JWT token's payload looks and gengerates on the backend
 
     // On initial load, check if token exists
     useEffect(() => {
@@ -15,8 +17,8 @@ export const AuthProvider = ({ children }) => {
             // You might want to decode the token to get user info
             try {
                 const decodedToken = jwtDecode(token); // requires jwt-decode library
-                const username = decodedToken.username || decodedToken.name || decodedToken.sub  //set user data from token
-                setUser({ username: username })
+                const username = decodedToken?.sub?.username || decodedToken?.username || decodedToken?.name //set user data from token
+                setUser({ username })
                 setIsLoggedIn(true);
             } catch (error) {
                 console.error("Failed to decode token or token is invalid:", error);
@@ -45,8 +47,8 @@ export const AuthProvider = ({ children }) => {
             // Otherwise, decode from the token (ensure 'username' claim exists in your JWT)
             try {
                 const decodedToken = jwtDecode(token);
-                const username = decodedToken.username || decodedToken.name || decodedToken.sub
-                setUser({ username: username })
+                const username = decodedToken?.sub?.username || decodedToken?.username || decodedToken?.name
+                setUser({username })
             } catch (error) {
                 console.error("Failed to decode token on login:", error);
                 setUser(null); // Or set a default like { username: 'User' }
